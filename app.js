09d98f8331cba12f069620fdf39745e6e6c24eb6
Jelte Homminga <j.homminga@hypotheekbond.nl>
2020-08-23 15:50:32 +0200

    dinosaurs 🦕 🦖 on the grid

--- app.js
+++ app.js
@@ -1,32 +1,114 @@
+async function getData() {
+  const response = await fetch("/dino.json");
+  const { Dinos } = await response.json();
+  return Dinos;
+}
 
-    // Create Dino Constructor
+// Create Dino Constructor
+class Dino {
+  constructor({ species, weight, height, diet, where, when, fact }) {
+    this.species = species;
+    this.weight = weight;
+    this.height = height;
+    this.diet = diet;
+    this.where = where;
+    this.when = when;
+    this.fact = fact;
+  }
+}
 
+// Create Dino Objects
+const dinoObjects = async () => {
+  const Dinos = await getData();
+  console.log(Dinos.map((dino) => new Dino(dino)));
+};
 
-    // Create Dino Objects
+// Use IIFE to get human data from form
+const iife = (function () {
+  return async function () {
+    const dinos = await getData();
+    const name = document.getElementById("name").value;
+    const heightMeterValue = document.getElementById("meter").value;
+    const heightCentiMeterValue = document.getElementById("centimeter").value;
+    const height = heightMeterValue * 100 + heightCentiMeterValue;
+    const weight = document.getElementById("weight").value;
+    const diet = document.getElementById("diet").value.toLowerCase();
 
+    const dinoObjects = dinos.map((dino) => new Dino(dino));
 
-    // Create Human Object
+    let dinoHtml = "";
 
-    // Use IIFE to get human data from form
+    dinoObjects.forEach((dino, i) => {
+      const OptionOne = dino.compareWeight(weight);
+      const OptionTwo = dino.compareHeight(height);
+      const OptionThree = dino.compareDiet(diet);
 
+      const compareRandom = [OptionOne, OptionTwo, OptionThree][
+        Math.floor(Math.random() * 3)
+      ];
 
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
+      if (i !== 4) {
+        dinoHtml += addTile(dino, compareRandom);
+      } else {
+        dinoHtml +=
+          `<div class="grid-item">
+        <h3>Human</h3>
+        <h4>${name}</h4>
+          <img src="images/human.png"> 
+          </div>` + addTile(dino, compareRandom);
+      }
+    });
 
-    
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+    addTilesToDom(dinoHtml);
+    removeFormFromScreen();
+  };
+})();
 
-    
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+// Create Dino Compare Method 1
+// NOTE: Weight in JSON file is in lbs, height in inches.
+Dino.prototype.compareWeight = function (weight) {
+  return `The dino is ${weight < this.weight ? "heavier" : "lighter"} than you`;
+};
 
+// Create Dino Compare Method 2
+// NOTE: Weight in JSON file is in lbs, height in inches.
+Dino.prototype.compareHeight = function (height) {
+  return `The dino is ${height < this.height ? "taller" : "smaller"} than you`;
+};
 
-    // Generate Tiles for each Dino in Array
-  
-        // Add tiles to DOM
+// Create Dino Compare Method 3
+// NOTE: Weight in JSON file is in lbs, height in inches.
+Dino.prototype.compareDiet = function (diet) {
+  return diet !== this.diet
+    ? `The dino is not a ${diet} like you, but a ${this.diet}`
+    : `You are both ${diet}`;
+};
 
-    // Remove form from screen
+// Generate Tiles for each Dino in Array
+const grid = document.getElementById("grid");
 
+const addTile = (dino, compareText) => {
+  return `<div class="grid-item">
+  <h3>${dino.species}</h3>
+  <h4>${dino.fact}</h4>
+    <img src="images/${dino.species.toLowerCase()}.png"> 
+    <h4>${compareText}</h4>
+    </div>`;
+};
+
+// Add tiles to DOM
+const addTilesToDom = (tilesHtml) => {
+  const grid = document.getElementById("grid");
+  grid.innerHTML = tilesHtml;
+};
+
+// Remove form from screen
+const removeFormFromScreen = () => {
+  const form = document.getElementById("dino-compare");
+  form.parentNode.removeChild(form);
+};
 
 // On button click, prepare and display infographic
+const button = document.getElementById("btn");
+
+button.addEventListener("click", iife);
